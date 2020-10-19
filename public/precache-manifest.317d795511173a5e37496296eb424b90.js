47316431ea3e4e65b746bca3bf06a0ca58fe08b4
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-10-19 10:27:30 -0500

    with frontend build copied into public folder

---
+++ public/precache-manifest.317d795511173a5e37496296eb424b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405984d17b14ee844f2b0dec90f917a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1319a8ebe4867ff8224",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3ef0f690578db7928c23",
+    "url": "/static/css/main.2375decf.chunk.css"
+  },
+  {
+    "revision": "b1319a8ebe4867ff8224",
+    "url": "/static/js/2.20d26172.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/static/js/2.20d26172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef0f690578db7928c23",
+    "url": "/static/js/main.ed6572cd.chunk.js"
+  },
+  {
+    "revision": "cbede0f450d34044366a",
+    "url": "/static/js/runtime-main.a8652280.js"
+  }
+]);
